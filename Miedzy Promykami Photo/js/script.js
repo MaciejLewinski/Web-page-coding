32d76e42251e7238514265de19bdc5df46b9402f
MaciejLewinski <127107510+MaciejLewinski@users.noreply.github.com>
2023-05-16 20:27:34 +0200

    Add files via upload

--- Miedzy Promykami Photo/js/script.js
+++ Miedzy Promykami Photo/js/script.js
@@ -6,29 +6,61 @@ document.addEventListener('click', () => {
     }
 })
 
+// IMG VIEWER IN GALLERY
+
+// const options = {
+// 	keyboard: true,
+// 	size: 'fullscreen'
+// };
+
+// document.querySelectorAll('.my-lightbox-toggle').forEach((el) => el.addEventListener('click', (e) => {
+// 	e.preventDefault();
+// 	const lightbox = new Lightbox(el, options);
+// 	lightbox.show();
+// }));
+
 
 // INSTAGRAM FEED APP
 
-let userFeed = new Instafeed({
-    get: 'user',
-    target: "instafeed-container",
-    resolution: 'low_resolution',
-    // Limit pokazywanych Feedsów 
-    limit: 4,               
-    accessToken: 'API KEY'
-});
+// let userFeed = new Instafeed({
+//     get: 'user',
+//     target: "instafeed-container",
+//     resolution: 'low_resolution',
+//     // Limit pokazywanych Feedsów 
+//     limit: 4,    
+//     accessToken: 'IGQVJXXzIzbHUzcVVVd3BDY19XUjlMS215dTZANeGhJUFRWQzZAwYlVXUi1rMlpPanZAPcUtsLXNTeUJIUWlQSUJzM194YXlmUUVMZA3d4SVRITU02aktDTFdPaVZAGdzVISGJuNy1HcWctZAlZALS2xndXBKeQZDZD'
+// });
 
-userFeed.run()
-let userFeedSnd = new Instafeed({
-    get: 'user',
-    target: "instafeed-container-snd",
-    resolution: 'low_resolution',
-    // Limit pokazywanych Feedsów 
-    limit: 4,               
-    accessToken: 'IGQVJWWnRuQkkyc0R0UkxyWWpMYUhESzJGWjcxWmg3RnJJbGNRdzhEU3JRa3hMQzNUX01NSXpRZADdndE1fVmFPSXRXcVFQbTRhVDl4aDd6bHNPdDhXR2lGWEF4X1F6QVRvNi1qY0ZAJdTlLSDJtYkRTNgZDZD'
-});
+// userFeed.run()
+// // console.log(userFeed);
 
-userFeedSnd.run()
 
-console.log(userFeedSnd);
+
+
+// let userFeedSnd = new Instafeed({
+//     get: 'user',
+//     target: "instafeed-container-snd",
+//     resolution: 'low_resolution',
+//     // Limit pokazywanych Feedsów 
+//     limit: 8,
+//     template: "<a id=\"nextInsta\" href=\"{{link}}\"><img title=\"{{caption}}\" src=\"{{image}}\" /></a>",
+//     accessToken: 'IGQVJXMEUyWXFfMmJ6Vm9uU2F4TDdwdmFud3dnQi1zQ1pDMXBrSGt6N21PZA2N1R3JUTm5oMUJ0cEZAZAaU16bm9peGZATQzFsWlhIR0tDandoSTZAnSk1ZALVM1MHlMdnFUMDhTWnZAuUTRZAYjMtNzhSd2xiVQZDZD'
+// });
+
+// userFeedSnd.run()
+// console.log(userFeedSnd);
+
+// const element = document.getElementById("instafeed-container-snd");
+// console.log(element);
+// element.remove();
+
+// let i = 0
+// do {
+//     // i += 0;
+//     element.remove();
+// } while (i <= 4);
+
+// for ( const i = 0, len = nextInstaRemove.lenght; i < len; i++) {
+//     nextInstaRemove[i].remove();
+// }
 
